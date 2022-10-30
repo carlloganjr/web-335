4c95bff79f5e9f6929d275e8e65c59a3c8385e17
Carl Logan <carlloganjr@gmail.com>
2022-10-30 11:12:12 -0600

    added week 2

---
+++ week_2/Logan_myworld.py
@@ -0,0 +1,11 @@
+
+"""
+================================================================
+    Title: Logan_myworld.py
+    Author: Carl Logan
+    Date: 10/27/2022
+    Description: First python app creation.
+================================================================
+"""
+name = 'Logan'
+print("You are now in {}'s world!".format(name))
